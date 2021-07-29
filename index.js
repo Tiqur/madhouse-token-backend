538f144626ce035061a0f931aa80e56988aff4f6
Tiqur <Tiqur7@gmail.com>
2021-07-29 01:11:11 -0700

    Update serve directory

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("build"));
+app.use(express.static("./madhouse-token-frontend/build"));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
